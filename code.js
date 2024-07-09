4da223467b96aa0a39f6e48fa81f84f993ade13a
ramapoltek <158381338+ramapoltek@users.noreply.github.com>
2024-07-09 21:57:32 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,104 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(text) {
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hour = day.getHours();
+
+    if (hour >= 0 && hour < 12) {
+        speak("Good Morning Sir...");
+    } else if (hour >= 12 && hour < 17) {
+        speak("Good Afternoon Sir...");
+    } else {
+        speak("Good Evining Sir...");
+    }
+}
+
+window.addEventListener('load', () => {
+    speak("Initializing JARVIS...");
+    wishMe();
+});
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+let isSpeaking = false;
+
+recognition.onresult = (event) => {
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+};
+
+recognition.onend = () => {
+    if (!isSpeaking) {
+        recognition.start();
+    }
+};
+
+btn.addEventListener('click', () => {
+    content.textContent = "Listening...";
+    recognition.start();
+});
+
+function takeCommand(message) {
+    isSpeaking = true;
+    if (message.includes('hey') || message.includes('hello')) {
+        speak("Hello Sir, How May I Help You?");
+    } else if (message.includes("open google")) {
+        speak("Opening Google...");
+        setTimeout(() => {
+            window.open("https://google.com", "_blank");
+        }, 2000);
+    } else if (message.includes("open youtube")) {
+        speak("Opening Youtube...");
+        setTimeout(() => {
+            window.open("https://youtube.com", "_blank");
+        }, 2000);
+    } else if (message.includes("open facebook")) {
+        speak("Opening Facebook...");
+        setTimeout(() => {
+            window.open("https://facebook.com", "_blank");
+        }, 2000);
+    } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        speak("This is what I found on the internet regarding " + message);
+        setTimeout(() => {
+            window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        }, 2000);
+    } else if (message.includes('wikipedia')) {
+        speak("This is what I found on Wikipedia regarding " + message);
+        setTimeout(() => {
+            window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "").trim()}`, "_blank");
+        }, 2000);
+    } else if (message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
+        speak("The current time is " + time);
+    } else if (message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, { month: "short", day: "numeric" });
+        speak("Today's date is " + date);
+    } else if (message.includes('calculator')) {
+        speak("Opening Calculator");
+        setTimeout(() => {
+            window.open('Calculator:///', "_blank");
+        }, 2000);
+    } else {
+        speak("I found some information for " + message + " on Google");
+        setTimeout(() => {
+            window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        }, 2000);
+    }
+    setTimeout(() => {
+        isSpeaking = false;
+    }, 2000);
+}
